784bd72cf1ef0370934de628c9e3ab75c185e36e
Tung Dinh <tungxd301@gmail.com>
2024-04-18 16:52:51 -0700

    Changes

--- src/Config/EMSEndpointConfig.tsx
+++ src/Config/EMSEndpointConfig.tsx
@@ -1,4 +1,5 @@
-export const EMS_BASE_URL = "http://54.191.67.200:80/api/private/v1";
+export const EMS_BASE_URL =
+  "https://ems-elb-1782094448.us-west-2.elb.amazonaws.com/api/private/v1";
 
 export const EMS_QUERY_EMAILS_SENT_PER_WORKFLOW_ENDPOINT =
   EMS_BASE_URL + "/query-emails-sent-per-workflow";
