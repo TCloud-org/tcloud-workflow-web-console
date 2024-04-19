ac5816122ec934aa42a310a8fdbf077d8a916010
Tung Dinh <tungxd301@gmail.com>
2024-04-18 17:09:43 -0700

    Changes

--- src/Config/SCSEndpointConfig.tsx
+++ src/Config/SCSEndpointConfig.tsx
@@ -1,4 +1,5 @@
-export const SCS_BASE_URL = "http://54.184.120.31:80/api/private/v1";
+export const SCS_BASE_URL =
+  "https://scs-elb-566306758.us-west-2.elb.amazonaws.com/api/private/v1";
 export const SCS_ADD_AUTH_TOKEN_URL = SCS_BASE_URL + "/add-auth-token";
 export const SCS_GET_TOKENS_BY_CLIENT_ID =
   SCS_BASE_URL + "/get-tokens-by-client-id";
