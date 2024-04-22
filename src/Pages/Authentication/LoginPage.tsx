a2ae8fdde561ac558658870a7d9b3fa1d6d27c7d
Tung Dinh <tungxd301@gmail.com>
2024-04-22 15:53:31 -0700

    Changes

--- src/Pages/Authentication/LoginPage.tsx
+++ src/Pages/Authentication/LoginPage.tsx
@@ -9,7 +9,7 @@ import { AppButton } from "DataEntryComponents/AppButton";
 import { AppForm } from "DataEntryComponents/AppForm";
 import { AppLine } from "LayoutComponents/AppLine";
 import { AuthContent } from "LayoutComponents/AuthContent";
-import { Col, Flex, Form, Input, Typography, theme } from "antd";
+import { Col, Flex, Form, Input, Typography, message, theme } from "antd";
 import axios from "axios";
 import { login } from "features/auth/authSlice";
 import { useState } from "react";
@@ -19,6 +19,7 @@ import { useNavigate } from "react-router-dom";
 export const LoginPage = () => {
   const { token } = theme.useToken();
   const [form] = Form.useForm();
+  const [messageApi, contextHolder] = message.useMessage();
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -89,128 +90,134 @@ export const LoginPage = () => {
     };
     const res = await axios
       .post(AMS_SIGN_IN_ENDPOINT, formData)
-      .then((res) => res.data);
-    dispatch(login({ token: res.token, account: res.account }));
+      .then((res) => res.data)
+      .catch((err) => messageApi.error("Login failed. Please try again"));
+    if (res) {
+      dispatch(login({ token: res.token, account: res.account }));
+    }
 
     setEmailSignInLoading(false);
   };
 
   return (
-    <AuthContent>
-      <Col span={12}>
-        <AuthImageDisplay
-          imageUrl="https://tcw-images.s3.us-west-2.amazonaws.com/6225958.jpg"
-          resourceUrl="https://www.freepik.com/free-vector/hand-drawn-illustration-business-planning_20124625.htm#fromView=search&page=3&position=24&uuid=86237bfa-c9e7-4467-92b5-0606a5715ad3"
-          resourceLabel="Image by freepik"
-        />
-      </Col>
+    <>
+      {contextHolder}
+      <AuthContent>
+        <Col span={12}>
+          <AuthImageDisplay
+            imageUrl="https://tcw-images.s3.us-west-2.amazonaws.com/6225958.jpg"
+            resourceUrl="https://www.freepik.com/free-vector/hand-drawn-illustration-business-planning_20124625.htm#fromView=search&page=3&position=24&uuid=86237bfa-c9e7-4467-92b5-0606a5715ad3"
+            resourceLabel="Image by freepik"
+          />
+        </Col>
 
-      <Col span={12}>
-        <Flex
-          vertical
-          align="center"
-          gap={16}
-          justify="center"
-          style={{ height: "100%" }}
-        >
-          <AppLogoText />
-
-          <Typography.Title level={3}>Welcome</Typography.Title>
-          <AppForm
-            form={form}
-            wrapperCol={Span[1]}
-            onValuesChange={handleValuesChange}
-            style={{
-              width: "100%",
-              padding: "0 64px",
-              marginTop: 16,
-            }}
+        <Col span={12}>
+          <Flex
+            vertical
+            align="center"
+            gap={16}
+            justify="center"
+            style={{ height: "100%" }}
           >
-            <Form.Item name="email">
-              <Input
-                style={{
-                  padding: "16px",
-                  borderRadius: token.borderRadiusLG,
-                }}
-                placeholder="Email address"
-              />
-            </Form.Item>
-            <Form.Item name="password">
-              <Input.Password
-                style={{
-                  padding: "16px",
-                  borderRadius: token.borderRadiusLG,
-                }}
-                placeholder="Password"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Flex justify="flex-end">
-                <AppButton
-                  type="link"
-                  style={{ color: token.colorTextSecondary }}
-                  onClick={handleForgotPassword}
-                >
-                  Forgot your password?
-                </AppButton>
-              </Flex>
-            </Form.Item>
-            <Form.Item>
-              <AppButton
-                style={{
-                  backgroundColor: borderColor,
-                  color: token.colorWhite,
-                  width: "100%",
-                  fontSize: 14,
-                }}
-                size="large"
-                onClick={handleSignIn}
-                loading={emailSignInLoading}
-              >
-                Login
-              </AppButton>
-            </Form.Item>
-            <Form.Item>
-              <Flex gap={16} align="center">
-                <AppLine />
-                <div style={{ color: token.colorTextSecondary }}>OR</div>
-                <AppLine />
-              </Flex>
-            </Form.Item>
-            <Flex justify="center" align="center">
-              <Form.Item style={{ width: "100%" }}>
-                <GoogleLoginButton
-                  loading={googleSignInLoading}
+            <AppLogoText />
+
+            <Typography.Title level={3}>Welcome</Typography.Title>
+            <AppForm
+              form={form}
+              wrapperCol={Span[1]}
+              onValuesChange={handleValuesChange}
+              style={{
+                width: "100%",
+                padding: "0 64px",
+                marginTop: 16,
+              }}
+            >
+              <Form.Item name="email">
+                <Input
                   style={{
+                    padding: "16px",
+                    borderRadius: token.borderRadiusLG,
+                  }}
+                  placeholder="Email address"
+                />
+              </Form.Item>
+              <Form.Item name="password">
+                <Input.Password
+                  style={{
+                    padding: "16px",
+                    borderRadius: token.borderRadiusLG,
+                  }}
+                  placeholder="Password"
+                />
+              </Form.Item>
+              <Form.Item>
+                <Flex justify="flex-end">
+                  <AppButton
+                    type="link"
+                    style={{ color: token.colorTextSecondary }}
+                    onClick={handleForgotPassword}
+                  >
+                    Forgot your password?
+                  </AppButton>
+                </Flex>
+              </Form.Item>
+              <Form.Item>
+                <AppButton
+                  style={{
+                    backgroundColor: borderColor,
+                    color: token.colorWhite,
                     width: "100%",
                     fontSize: 14,
                   }}
                   size="large"
-                  initializeLoading={setGoogleSignInLoading}
-                  onSignInSuccess={handleGoogleSuccessSignIn}
-                  onSignInError={() => {
-                    console.log("Login Failed");
-                  }}
-                />
+                  onClick={handleSignIn}
+                  loading={emailSignInLoading}
+                >
+                  Login
+                </AppButton>
               </Form.Item>
-            </Flex>
-            <Form.Item>
-              <Flex justify="center">
-                <Typography.Text type="secondary">
-                  Don't have an account?{" "}
-                  <AppButton
-                    style={{ margin: 0, padding: 0 }}
-                    type="link"
-                    onClick={handleSignUp}
-                  >
-                    Sign Up
-                  </AppButton>
-                </Typography.Text>
+              <Form.Item>
+                <Flex gap={16} align="center">
+                  <AppLine />
+                  <div style={{ color: token.colorTextSecondary }}>OR</div>
+                  <AppLine />
+                </Flex>
+              </Form.Item>
+              <Flex justify="center" align="center">
+                <Form.Item style={{ width: "100%" }}>
+                  <GoogleLoginButton
+                    loading={googleSignInLoading}
+                    style={{
+                      width: "100%",
+                      fontSize: 14,
+                    }}
+                    size="large"
+                    initializeLoading={setGoogleSignInLoading}
+                    onSignInSuccess={handleGoogleSuccessSignIn}
+                    onSignInError={() => {
+                      console.log("Login Failed");
+                    }}
+                  />
+                </Form.Item>
               </Flex>
-            </Form.Item>
-          </AppForm>
-        </Flex>
-      </Col>
-    </AuthContent>
+              <Form.Item>
+                <Flex justify="center">
+                  <Typography.Text type="secondary">
+                    Don't have an account?{" "}
+                    <AppButton
+                      style={{ margin: 0, padding: 0 }}
+                      type="link"
+                      onClick={handleSignUp}
+                    >
+                      Sign Up
+                    </AppButton>
+                  </Typography.Text>
+                </Flex>
+              </Form.Item>
+            </AppForm>
+          </Flex>
+        </Col>
+      </AuthContent>
+    </>
   );
 };
--- src/Pages/Authentication/SignUpPage.tsx
+++ src/Pages/Authentication/SignUpPage.tsx
@@ -1,3 +1,4 @@
+import { AMS_SIGN_UP_ENDPOINT } from "Config/AMSEndpointConfig";
 import { borderColor } from "Config/AutomationConfig";
 import { Span } from "Config/DataDisplayInterface";
 import { AppLogoText } from "DataDisplayComponents/AppLogoText";
@@ -5,67 +6,123 @@ import { AuthImageDisplay } from "DataDisplayComponents/AuthImageDisplay";
 import { AppButton } from "DataEntryComponents/AppButton";
 import { AppForm } from "DataEntryComponents/AppForm";
 import { AuthContent } from "LayoutComponents/AuthContent";
-import { Checkbox, Col, Flex, Form, Input, Typography, theme } from "antd";
+import {
+  Checkbox,
+  Col,
+  Flex,
+  Form,
+  Input,
+  Typography,
+  message,
+  theme,
+} from "antd";
+import axios from "axios";
+import { login } from "features/auth/authSlice";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export const SignUpPage = () => {
   const { token } = theme.useToken();
+  const [form] = Form.useForm();
+  const [messageApi, contextHolder] = message.useMessage();
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [emailSignUpLoading, setEmailSignUpLoading] = useState<boolean>(false);
 
   const handleLogin = () => {
     navigate("/");
   };
 
+  const handleValuesChange = (_: any, values: any) => {
+    form.setFieldsValue(values);
+  };
+
+  const handleSignUp = async () => {
+    setEmailSignUpLoading(true);
+
+    const formData = {
+      email: form.getFieldValue("email"),
+      password: form.getFieldValue("password"),
+      firstName: form.getFieldValue("firstName"),
+      lastName: form.getFieldValue("lastName"),
+    };
+    const res = await axios
+      .post(AMS_SIGN_UP_ENDPOINT, formData)
+      .then((res) => res.data)
+      .catch((err) => messageApi.error("Sign up failed. Please try again"));
+    if (res) {
+      dispatch(login({ token: res.token, account: res.account }));
+      navigate("/");
+    }
+    setEmailSignUpLoading(false);
+  };
+
   return (
-    <AuthContent>
-      <Col span={12}>
-        <AuthImageDisplay
-          imageUrl="https://tcw-images.s3.us-west-2.amazonaws.com/6221508.jpg"
-          resourceUrl="https://www.freepik.com/free-vector/hand-drawn-innovation-concept_20286043.htm#&position=8&from_view=collections&uuid=5a145899-aec1-4149-aaa3-f325d2281b06"
-          resourceLabel="Image by pikisuperstar on Freepik"
-        />
-      </Col>
+    <>
+      {contextHolder}
+      <AuthContent>
+        <Col span={12}>
+          <AuthImageDisplay
+            imageUrl="https://tcw-images.s3.us-west-2.amazonaws.com/6221508.jpg"
+            resourceUrl="https://www.freepik.com/free-vector/hand-drawn-innovation-concept_20286043.htm#&position=8&from_view=collections&uuid=5a145899-aec1-4149-aaa3-f325d2281b06"
+            resourceLabel="Image by pikisuperstar on Freepik"
+          />
+        </Col>
 
-      <Col span={12}>
-        <Flex
-          vertical
-          align="center"
-          gap={16}
-          justify="center"
-          style={{ height: "100%" }}
-        >
-          <AppLogoText />
-
-          <Typography.Title level={3}>Welcome</Typography.Title>
-          <AppForm
-            wrapperCol={Span[1]}
-            style={{
-              width: "100%",
-              padding: "0 64px",
-              marginTop: 16,
-            }}
+        <Col span={12}>
+          <Flex
+            vertical
+            align="center"
+            gap={16}
+            justify="center"
+            style={{ height: "100%" }}
           >
-            <Form.Item>
-              <Input
-                style={{
-                  padding: "16px",
-                  borderRadius: token.borderRadiusLG,
-                }}
-                placeholder="Full name"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Input
-                style={{
-                  padding: "16px",
-                  borderRadius: token.borderRadiusLG,
-                }}
-                placeholder="Email address"
-              />
-            </Form.Item>
-            <Flex align="center" justify="space-between" gap={16}>
-              <Form.Item style={{ flex: 1 }}>
+            <AppLogoText />
+
+            <Typography.Title level={3}>Welcome</Typography.Title>
+            <AppForm
+              onValuesChange={handleValuesChange}
+              form={form}
+              wrapperCol={Span[1]}
+              style={{
+                width: "100%",
+                padding: "0 64px",
+                marginTop: 16,
+              }}
+            >
+              <Flex gap={20} align="center">
+                <Form.Item style={{ flex: 1 }} name="firstName">
+                  <Input
+                    style={{
+                      padding: "16px",
+                      borderRadius: token.borderRadiusLG,
+                    }}
+                    placeholder="First name"
+                  />
+                </Form.Item>
+                <Form.Item style={{ flex: 1 }} name="lastName">
+                  <Input
+                    style={{
+                      padding: "16px",
+                      borderRadius: token.borderRadiusLG,
+                    }}
+                    placeholder="Last name"
+                  />
+                </Form.Item>
+              </Flex>
+              <Form.Item name="email">
+                <Input
+                  style={{
+                    padding: "16px",
+                    borderRadius: token.borderRadiusLG,
+                  }}
+                  placeholder="Email address"
+                />
+              </Form.Item>
+              <Form.Item style={{ flex: 1 }} name="password">
                 <Input.Password
                   style={{
                     padding: "16px",
@@ -74,54 +131,47 @@ export const SignUpPage = () => {
                   placeholder="Password"
                 />
               </Form.Item>
-              <Form.Item style={{ flex: 1 }}>
-                <Input.Password
-                  style={{
-                    padding: "16px",
-                    borderRadius: token.borderRadiusLG,
-                  }}
-                  placeholder="Confirmed password"
-                />
-              </Form.Item>
-            </Flex>
-            <Form.Item>
               <Flex justify="flex-start">
-                <Checkbox style={{ color: token.colorTextSecondary }}>
-                  I agree to the <a href="/terms-and-conditions">Term</a> &{" "}
-                  <a href="privacy-policy">Privacy</a>
-                </Checkbox>
+                <Form.Item>
+                  <Checkbox style={{ color: token.colorTextSecondary }}>
+                    I agree to the <a href="/terms-and-conditions">Term</a> &{" "}
+                    <a href="privacy-policy">Privacy</a>
+                  </Checkbox>
+                </Form.Item>
               </Flex>
-            </Form.Item>
-            <Form.Item>
-              <AppButton
-                style={{
-                  backgroundColor: borderColor,
-                  color: token.colorWhite,
-                  width: "100%",
-                  fontSize: 14,
-                }}
-                size="large"
-              >
-                Sign Up
-              </AppButton>
-            </Form.Item>
-            <Form.Item>
-              <Flex justify="center">
-                <Typography.Text type="secondary">
-                  Already have an account?{" "}
-                  <AppButton
-                    style={{ margin: 0, padding: 0 }}
-                    type="link"
-                    onClick={handleLogin}
-                  >
-                    Login
-                  </AppButton>
-                </Typography.Text>
-              </Flex>
-            </Form.Item>
-          </AppForm>
-        </Flex>
-      </Col>
-    </AuthContent>
+              <Form.Item>
+                <AppButton
+                  style={{
+                    backgroundColor: borderColor,
+                    color: token.colorWhite,
+                    width: "100%",
+                    fontSize: 14,
+                  }}
+                  size="large"
+                  onClick={handleSignUp}
+                  loading={emailSignUpLoading}
+                >
+                  Sign Up
+                </AppButton>
+              </Form.Item>
+              <Form.Item>
+                <Flex justify="center">
+                  <Typography.Text type="secondary">
+                    Already have an account?{" "}
+                    <AppButton
+                      style={{ margin: 0, padding: 0 }}
+                      type="link"
+                      onClick={handleLogin}
+                    >
+                      Login
+                    </AppButton>
+                  </Typography.Text>
+                </Flex>
+              </Form.Item>
+            </AppForm>
+          </Flex>
+        </Col>
+      </AuthContent>
+    </>
   );
 };
