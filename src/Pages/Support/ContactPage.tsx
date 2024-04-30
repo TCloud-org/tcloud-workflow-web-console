b044bd949f8a1bc9f552091155897067a723ed49
Tung Dinh <tungxd301@gmail.com>
2024-04-30 00:25:33 -0700

    Changes

--- src/Pages/Support/ContactPage.tsx
+++ src/Pages/Support/ContactPage.tsx
@@ -1,9 +1,32 @@
-import { ArrowRightOutlined } from "@ant-design/icons";
+import {
+  ArrowRightOutlined,
+  EnvironmentOutlined,
+  MailOutlined,
+  PhoneOutlined,
+} from "@ant-design/icons";
+import { PageTitle } from "DataDisplayComponents/PageTitle";
 import { AppButton } from "DataEntryComponents/AppButton";
 import { AppForm } from "DataEntryComponents/AppForm";
-import { Flex, Form, Input, Typography } from "antd";
+import { AppRow } from "LayoutComponents/AppRow";
+import { Card, Col, Flex, Form, Input, Typography, theme } from "antd";
 
+const contactOptions = [
+  {
+    icon: <PhoneOutlined />,
+    value: "+1 (425) 229-8028",
+  },
+  {
+    icon: <MailOutlined />,
+    value: "thecloudworld24@outlook.com",
+  },
+  {
+    icon: <EnvironmentOutlined />,
+    value: "Bellevue, WA",
+  },
+];
 export const ContactPage = () => {
+  const { token } = theme.useToken();
+
   return (
     <div
       style={{
@@ -12,36 +35,60 @@ export const ContactPage = () => {
         height: "100%",
       }}
     >
-      <Flex
-        align="center"
-        justify="center"
-        style={{ height: "100%", flex: 1 }}
-        gap={32}
-      >
-        <div style={{ flex: 1 }}></div>
-        <Flex vertical style={{ flex: 1, width: "100%" }} gap={16}>
-          <Typography.Title level={3}>Contact us</Typography.Title>
-          <AppForm layout="vertical" style={{ width: "100%" }}>
-            <Flex align="center" gap={16}>
-              <Form.Item
-                style={{ flex: 1 }}
-                label="First name"
-                name="firstName"
-              >
+      <PageTitle>Contact us</PageTitle>
+      <AppRow style={{ height: "100%", flex: 1 }} gutter={[16, 16]}>
+        <Col span={12}>
+          <Flex vertical gap={16} align="flex-start">
+            <Typography.Text>
+              Any question? We would be happy to help
+            </Typography.Text>
+            {contactOptions.map((option, i) => (
+              <Card style={{ width: 450 }} hoverable key={i}>
+                <Flex gap={16}>
+                  {option.icon}
+                  <Typography.Text>{option.value}</Typography.Text>
+                </Flex>
+              </Card>
+            ))}
+          </Flex>
+        </Col>
+        <Col span={12}>
+          <Flex
+            vertical
+            gap={16}
+            style={{
+              padding: 32,
+              boxShadow: token.boxShadowSecondary,
+              borderRadius: token.borderRadiusLG,
+            }}
+          >
+            <Typography.Title level={3} style={{ marginTop: 0 }}>
+              Get in touch
+            </Typography.Title>
+            <AppForm layout="vertical" style={{ width: "100%" }}>
+              <Flex align="center" gap={16}>
+                <Form.Item
+                  style={{ flex: 1 }}
+                  label="First name"
+                  name="firstName"
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item
+                  style={{ flex: 1 }}
+                  label="Last name"
+                  name="lastName"
+                >
+                  <Input />
+                </Form.Item>
+              </Flex>
+              <Form.Item label="Email address" name="email">
                 <Input />
               </Form.Item>
-              <Form.Item style={{ flex: 1 }} label="Last name" name="lastName">
-                <Input />
+              <Form.Item label="Message" name="message">
+                <Input.TextArea autoSize={{ minRows: 5, maxRows: 10 }} />
               </Form.Item>
-            </Flex>
-            <Form.Item label="Email address" name="email">
-              <Input />
-            </Form.Item>
-            <Form.Item label="Message" name="message">
-              <Input.TextArea autoSize={{ minRows: 5, maxRows: 10 }} />
-            </Form.Item>
 
-            <Flex>
               <Form.Item style={{ flex: 1 }}>
                 <AppButton
                   type="primary"
@@ -51,10 +98,10 @@ export const ContactPage = () => {
                   Submit
                 </AppButton>
               </Form.Item>
-            </Flex>
-          </AppForm>
-        </Flex>
-      </Flex>
+            </AppForm>
+          </Flex>
+        </Col>
+      </AppRow>
     </div>
   );
 };
