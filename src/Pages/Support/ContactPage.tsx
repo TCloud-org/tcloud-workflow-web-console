324534320e161889a3d724113e03f97a5b978533
Tung Dinh <tungxd301@gmail.com>
2024-04-29 23:26:15 -0700

    Changes

--- src/Pages/Support/ContactPage.tsx
+++ src/Pages/Support/ContactPage.tsx
@@ -2,46 +2,8 @@ import { ArrowRightOutlined } from "@ant-design/icons";
 import { AppButton } from "DataEntryComponents/AppButton";
 import { AppForm } from "DataEntryComponents/AppForm";
 import { Flex, Form, Input, Typography } from "antd";
-import DottedMap from "dotted-map";
-
-const map = new DottedMap({ height: 150, grid: "vertical" });
-
-map.addPin({
-  lat: 47.610378,
-  lng: -122.200676,
-  data: "Point 1",
-});
-
-const points = map.getPoints();
-const pins = points.filter((point) => point.data);
-
-const svgOptions = {
-  backgroundColor: "#FFFFFF",
-  color: "#000000",
-  radius: 0.35,
-};
-
-// const svgMap = map.getSVG({
-//   ...svgOptions,
-//   shape: "circle",
-// });
-
-const size = {
-  x: 100,
-  y: 100,
-};
-
-const offset = {
-  x: 25,
-  y: 2,
-};
 
 export const ContactPage = () => {
-  const pin = pins[0];
-  const viewBox = `${pin.x - size.x / 2 + offset.x} ${
-    pin.y - size.y / 2 + offset.y
-  } ${size.x} ${size.y}`;
-
   return (
     <div
       style={{
@@ -56,23 +18,7 @@ export const ContactPage = () => {
         style={{ height: "100%", flex: 1 }}
         gap={32}
       >
-        <div style={{ flex: 1 }}>
-          <svg
-            viewBox={viewBox}
-            style={{ background: svgOptions.backgroundColor }}
-          >
-            {points.map((point, i) => (
-              <circle
-                key={i}
-                cx={point.x}
-                cy={point.y}
-                r={svgOptions.radius}
-                fill={svgOptions.color}
-                style={{ opacity: pin.data === point.data ? 1 : 0.25 }}
-              />
-            ))}
-          </svg>
-        </div>
+        <div style={{ flex: 1 }}></div>
         <Flex vertical style={{ flex: 1, width: "100%" }} gap={16}>
           <Typography.Title level={3}>Contact us</Typography.Title>
           <AppForm layout="vertical" style={{ width: "100%" }}>
