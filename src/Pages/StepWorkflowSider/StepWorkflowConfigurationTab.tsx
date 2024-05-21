2d8e36a2a3760666195193614809600592c6ddf4
Tung Dinh <tungxd301@gmail.com>
2024-05-21 00:41:37 -0700

    Changes

--- src/Pages/StepWorkflowSider/StepWorkflowConfigurationTab.tsx
+++ src/Pages/StepWorkflowSider/StepWorkflowConfigurationTab.tsx
@@ -3,13 +3,34 @@ import { WorkflowPage } from "Pages/ApiWorkflowConfigurationSider/WorkflowPage";
 import { GraphPage } from "Pages/GraphSider/GraphPage";
 import { RetryPolicyPage } from "Pages/RetryPolicySider/RetryPolicyPage";
 import { ServicePage } from "Pages/ServiceSider/ServicePage";
+import { TabsProps } from "antd";
+import { useEffect, useState } from "react";
 
 export const StepWorkflowConfigurationTab = () => {
+  const [tabPosition, setTabPosition] =
+    useState<TabsProps["tabPosition"]>("left");
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setTabPosition("top");
+      } else {
+        setTabPosition("left");
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <AppMainTabs
       defaultActiveKey="workflow"
       tabindexkey="configurationTab"
-      tabPosition="left"
+      tabPosition={tabPosition}
       items={[
         {
           key: "workflow",
