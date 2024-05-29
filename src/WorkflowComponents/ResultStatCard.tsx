eaff9c2526e07da689ba8786e36adc481e22a9b2
Tung Dinh <tungxd301@gmail.com>
2024-05-29 01:00:59 -0700

    Changes

--- src/WorkflowComponents/ResultStatCard.tsx
+++ src/WorkflowComponents/ResultStatCard.tsx
@@ -61,7 +61,10 @@ export const ResultStatCard = (props: {
           className="z-10 bg-white"
         />
       </AppCard>
-      <div className="absolute left-[12px] right-0 top-[8px] -bottom-[8px] bg-primary/35 border border-black rounded-md z-0" />
+      <div
+        className="absolute left-[12px] right-0 top-[8px] -bottom-[8px] bg-primary/35 border border-black rounded-md z-0"
+        style={{ backgroundColor: data.color, opacity: 0.5 }}
+      />
     </Fragment>
   );
 };
